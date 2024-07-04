4d219e3306002e9c05159219b1a33c2b3cb04084
Ashishk7011 <101982313+Ashishk7011@users.noreply.github.com>
2024-07-04 12:45:12 +0530

    Delete project.sql

--- project.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM ashish.project;
-
-select Outcome, count(Outcome) from project
-group by Outcome;
-
-select Outcome, avg(Glucose), avg(bmi), avg(BloodPressure), avg(SkinThickness), avg(Insulin), avg(DiabetesPedigreeFunction), avg(Age) from project
-group by Outcome;
