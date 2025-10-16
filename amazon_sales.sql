b30aca2c9cc5f75980d11e1eef08cfe7c86876bc
Ashishk7011 <101982313+Ashishk7011@users.noreply.github.com>
2025-10-16 13:16:46 +0530

    Update top_3_query.sql

--- amazon_sales.sql
+++ amazon_sales.sql
@@ -1,67 +1,47 @@
-select * from amazon_sales;
-
--- total sales by size
-select Size, sum(sales) from amazon_sales
-group by Size
-order by 2 desc;
-
--- total sales by size and category
-select Size, Category, sum(sales) from amazon_sales
-group by Size, Category
-order by 3 desc;
-
-select Size, `ship-state`, sum(sales) from amazon_sales
-group by Size, `ship-state`
-order by 3 desc;
-
-select `Date`, str_to_date(`Date`, '%m/%d/%Y') from amazon_sales;
-
--- change the datatype of `date` column text to date(m/d/Y); 
-update amazon_sales
-set `Date`= str_to_date(`Date`, '%m/%d/%Y');   
-
--- total sales by date(day and month) and category
-select day(`Date`) `day`, monthname(`Date`) `month`, Category, sum(sales) from amazon_sales
-group by Category, `day`, `month`
-order by 4 desc;
-
-select distinct `Courier Status` from amazon_sales;
-
--- total sales by courier status
-select `Courier Status`, avg(Amount), sum(sales) from amazon_sales
-group by `Courier Status`
-order by `Courier Status`;
-
--- using having clause for condition of courier status which is unshipped 
-select Category, `Courier Status`, sum(sales) total from amazon_sales
-group by Category, `Courier Status`
-having `Courier Status` = 'Unshipped';
-
-select Category, `Courier Status`, sum(sales) as total from amazon_sales
-group by Category, `Courier Status`
-having `Courier Status` = 'Unshipped'
-and total>1000000;
-
-select `ship-state` from (select `ship-state`, sum(sales) from amazon_sales group by `ship-state` order by 2 desc ) as table2;
-
-select * from amazon_sales
-order by sales desc
-limit 10;
+CREATE TABLE search_logs (
+    log_id INT PRIMARY KEY,
+    query VARCHAR(100),
+    timestamp DATETIME,
+    user_id VARCHAR(10)
+);
 
 
+INSERT INTO search_logs (log_id, query, timestamp, user_id) VALUES
+(1, 'data analyst', '2025-06-03 10:00:00', 'U101'),
+(2, 'SQL basics', '2025-06-03 11:00:00', 'U102'),
+(3, 'data analyst', '2025-06-04 09:30:00', 'U103'),
+(4, 'Python', '2025-06-05 14:00:00', 'U101'),
+(5, 'data analyst', '2025-06-05 16:00:00', 'U102'),
+(6, 'SQL basics', '2025-06-06 10:00:00', 'U101'),
+(7, 'Python', '2025-06-06 11:00:00', 'U103'),
+(8, 'machine learning', '2025-06-07 10:00:00', 'U101'),
+(9, 'data analyst', '2025-06-10 09:00:00', 'U102'),
+(10, 'SQL advanced', '2025-06-10 10:00:00', 'U101'),
+(11, 'SQL advanced', '2025-06-11 11:00:00', 'U103'),
+(12, 'Python', '2025-06-11 12:00:00', 'U102'),
+(13, 'data analyst', '2025-06-12 13:00:00', 'U101'),
+(14, 'machine learning', '2025-06-13 14:00:00', 'U103'),
+(15, 'Python', '2025-06-13 15:00:00', 'U101'),
+(16, 'data visualization', '2025-06-14 16:00:00', 'U102');
 
+select * from search_logs;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+WITH weekly_counts AS (
+    SELECT 
+        WEEK(timestamp) AS week_of_year,
+        query,
+        COUNT(query) AS query_count
+    FROM search_logs
+    GROUP BY week_of_year, query
+)
+SELECT 
+    week_of_year,
+    query,
+    query_count, rn from (SELECT 
+    week_of_year,
+    query,
+    query_count,
+    ROW_NUMBER() OVER (PARTITION BY week_of_year ORDER BY query_count DESC) AS rn
+FROM weekly_counts
+ORDER BY week_of_year, rn) as query_ranked
+where rn<=3;
